4f7b2feaf0c0c0b6e7e7fdbcb1b3d57d4815ce3d
osheen07 <osheenshrivastav@gmail.com>
2017-04-16 19:15:09 -0400

    Added border to the text box as per the messages Signed-off-by: osheen07 <osheenshrivastav@gmail.com>

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ function LunchCheckerController($scope) {
   $scope.message = "List items in the text box as per the instructions and click on the button to check if it is too much!";
   $scope.items = "";
   $scope.customStyle = {};
+  $scope.customStyleButton = {};
 //  $scope.itemsArray = $scope.splitItems();
 
   $scope.checkIfTooMuch = function () {
@@ -29,10 +30,12 @@ function LunchCheckerController($scope) {
       $scope.message = "Enjoy!";
     }
     $scope.customStyle.style = {"color": "green"};
+    $scope.customStyleButton.style = {"border": "3px solid green"};
   }
   else {
     $scope.message = "Please enter data first.";
     $scope.customStyle.style = {"color": "red"};
+    $scope.customStyleButton.style = {"border": "3px solid red"};
   }
   };
 };
